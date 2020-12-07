db564354f9b52f0489a8a04a8d5ccbf8b1beae12
Dmi3yy <dmi3yy@gmail.com>
2020-12-07 21:06:39 +0200

    use Content and TV

--- src/TinyMCE5ServiceProvider.php
+++ src/TinyMCE5ServiceProvider.php
@@ -20,13 +20,48 @@ class TinyMCE5ServiceProvider extends ServiceProvider
 
         Event::listen('evolution.OnRichTextEditorInit', function ($params) {
 
-            //echo print_r($elements, 1);
+            $selectorArr = array_map(function($val) { return '#'.$val;} , $params['elements']);
+
+            //echo print_r($selectorArr, 1);
+
             $script = "
 			<script src='".MODX_SITE_URL."assets/plugins/tinymce5/js/tinymce/tinymce.min.js'></script>
 			<script>
+			    let selectors = '".implode(',', $selectorArr). "';
+			    let modx_site_url = '" . MODX_SITE_URL . "';
+			    let filePicker = function (callback, value, meta) {
+               
+                    let type = 'images';
+                    if (meta.filetype == 'file') { type = 'files';}
+                    
+                    let windowManagerURL = '/manager/media/browser/mcpuk/browse.php?opener=tinymce4&field=src&type=' + type
+                    ;// filemanager path
+                    window.tinymceCallBackURL = '';
+                    window.tinymceWindowManager = tinymce.activeEditor.windowManager;
+                
+                    tinymce.activeEditor.windowManager.open({
+                        title: 'Image',
+                        size: 'large',
+                        body: {
+                        type: 'panel',
+                            items: [{
+                                type: 'htmlpanel',
+                                html: '<iframe src=\"' + windowManagerURL + '\" frameborder=\"0\" style=\"width:840px; height:500px\"></iframe>'
+                            }]
+                        },
+                        buttons: [
+                        ] ,
+                    onClose: function () {
+                        if (tinymceCallBackURL!='')
+                            callback(tinymceCallBackURL, {});
+                        } 
+                    });
+                }
+            </script>
+			<script>
                 tinymce.init({
-                  selector: '#ta',
-                  document_base_url:'".MODX_SITE_URL."',
+                  selector: selectors,
+                  document_base_url: modx_site_url,
                   language: 'ru',
                   plugins: 'print preview importcss searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
                   
@@ -54,34 +89,9 @@ class TinyMCE5ServiceProvider extends ServiceProvider
                   a11y_advanced_options: true,
                   skin: 'oxide',
                   content_css: 'default',
-                  file_picker_callback: function (callback, value, meta) {
-                  
-                    var type = 'images';
-                    if (meta.filetype == 'file') { type = 'files';}
-                    
-                    var windowManagerURL = '/manager/media/browser/mcpuk/browse.php?opener=tinymce4&field=src&type=' + type
-                    ;// filemanager path
-                    window.tinymceCallBackURL = '';
-                    window.tinymceWindowManager = tinymce.activeEditor.windowManager;
-                
-                    tinymce.activeEditor.windowManager.open({
-                        title: 'Image',
-                        size: 'large',
-                        body: {
-                        type: 'panel',
-                            items: [{
-                                type: 'htmlpanel',
-                                html: '<iframe src=\"' + windowManagerURL + '\" frameborder=\"0\" style=\"width:840px; height:500px\"></iframe>'
-                            }]
-                        },
-                        buttons: [
-                        ] ,
-                    onClose: function () {
-                        if (tinymceCallBackURL!='')
-                            callback(tinymceCallBackURL, {});
-                        } 
-                    });
-                }
+                  file_picker_callback: function(callback, value, meta) {
+                    filePicker(callback, value, meta)
+                  }
                 });
 			</script>";
             return $script;
