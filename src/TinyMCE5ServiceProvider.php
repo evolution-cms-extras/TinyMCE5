21f818335d4066baceb6a132091371aeb822e43a
Dmi3yy <dmi3yy@gmail.com>
2021-02-15 14:51:04 +0300

    update to 5.7.0

--- src/TinyMCE5ServiceProvider.php
+++ src/TinyMCE5ServiceProvider.php
@@ -44,7 +44,7 @@ class TinyMCE5ServiceProvider extends ServiceProvider
             }
 
             $config = "
-                <script src='" . MODX_SITE_URL . "core/vendor/tinymce/tinymce/tinymce.min.js'></script>
+                <script src='" . MODX_SITE_URL . "assets/plugins/tinymce5/tinymce/tinymce.min.js'></script>
                 <script>
                     let modx_site_url = '" . MODX_SITE_URL . "';
                     let lang = '" . evo()->getConfig('fe_editor_lang') . "';
